ae182c634e417870e776fc38bfb365203d2b7806
Nate Foster <nate.s.foster@gmail.com>
2023-01-17 18:49:45 -1000

    import db modules

--- db/etl.js
+++ db/etl.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
+const mongodb = require('./index.js');
+const sqldb = require('./sql/models.js');
 
 fs.createReadStream(path.join(__dirname, '../../data/characteristics.csv'))
 .pipe(parse({ delimiter: ',', from_line: 2 }))
 .on('data', (row) => {
   console.log(row);
-});
+})
