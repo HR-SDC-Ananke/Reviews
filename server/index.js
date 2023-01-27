4110ad2ace00653e5db6b97ada277b7b022d4c10
Nate Foster <nate.s.foster@gmail.com>
2023-01-27 09:12:15 -1000

    use morgan middleware for logging requests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const morgan = require('morgan');
+
 const app = express();
+
+// middleware for logging requests to the console
+app.use(morgan('tiny'));
 
 const port = process.env.PORT || 5000;
 app.listen(() => {
