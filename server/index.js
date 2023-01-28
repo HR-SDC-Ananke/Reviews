c4158bed06497013d573b3bafb86da91b2701f99
Nate Foster <nate.s.foster@gmail.com>
2023-01-27 17:03:57 -1000

    handle query param logic in reviews route

--- server/index.js
+++ server/index.js
@@ -12,17 +12,33 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 // connect to the database
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/sdc-reviews');
 
-app.get('/reviews/:product_id', async (req, res) => {
-  console.log(`req params: ${JSON.stringify(req.params)}`);
-  const reviews = await mongodb.Review.find({
-    product_id: req.params.product_id
-  })
-  .catch(err => {
-    console.log(`error retrieving reviews for product ${product_id}`, err);
-    res.sendStatus(500);
-  });
+app.get('/reviews/', async (req, res) => {
+  console.log(`req params: ${JSON.stringify(req.query)}`);
+  const count = req.query.count || 5;
+  const page = req.query.page || 1;
+  const sort = req.query.sort || "newest";
+  const product_id = req.query.product_id;
+  let reviews;
+
+  if (!product_id) {
+    reviews = await mongodb.Review.find({}).catch(err => res.sendStatus(400));
+  } else {
+    reviews = await mongodb.Review.find({ product_id }).catch(err => res.sendStatus(400));
+  }
+
+  const sortBy = (a, b, descriptor) => {
+    if (descriptor === 'newest') return a.date - b.date;
+    if (descriptor === 'helpful') return a.helpfulness - b.helpfulness;
+    return 0;
+  }
+
+  reviews.sort((a, b) => sortBy(a, b, sort));
+  if (reviews.length > (page - 1) * count) {
+    reviews = reviews.slice((page - 1) * count, page * count);
+  }
 
   !reviews.length ? res.sendStatus(400) : res.status(200).send(reviews);
 });
--- testing/server.test.js
+++ testing/server.test.js
@@ -12,15 +12,16 @@ describe('Reviews', () => {
   describe('Get /reviews/', () => {
     const url = `http://localhost:${process.env.PORT}`;
     it('should return all reviews for a given product id', async () => {
-      const response = await axios.get(`${url}/reviews/1`)
-      .catch(err => console.log(err));
+      const response = await axios.get(`${url}/reviews/?product_id=1`)
+      .catch(err => console.log(`error getting product with id 1`));
       expect(response.data.length).toBe(2);
       expect(response.status).toBe(200);
     });
     it('should send a status code of 400 for a bad request', async () => {
-      const response = await axios.get(`${url}/reviews/-3`)
+      const response = await axios.get(`${url}/reviews/?product_id=-3`)
       .catch(err => expect(err.response.status).toBe(400));
       expect(response).toBe(undefined);
-    })
+    });
+    it.todo('should default to page 1 and count 5 when given no query params');
   });
 });
