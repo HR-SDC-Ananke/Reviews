a669087fba194fbbcb6511e07118741f22c35133
Nate Foster <nate.s.foster@gmail.com>
2023-02-06 12:02:29 -1000

    change indexes to true

--- testing/k6productMeta.test.js
+++ testing/k6productMeta.test.js
@@ -2,19 +2,16 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 const productIds = require('../exampleData/productIds.js');
 
-// TODO
-// 1. Generate set of valid product_ids
-// 2. Create multiple scenarios using vus and sleeping
-// 3. Make test files for each get request
+const target = 1; // requests per second
 
 export const options = {
   scenarios: {
     stress: {
       executor: 'ramping-vus',
       stages: [
-        { duration: '5s', target: 10000 },
-        { duration: '10s', target: 10000 },
-        { duration: '5s', target: 0 }
+        { duration: '10s', target },
+        { duration: '30s', target },
+        { duration: '10s', target: 0 }
       ]
     }
   },
@@ -26,19 +23,11 @@ export const options = {
 
 export default function () {
   const url = 'http://localhost:5000';
-
   const id = productIds[Math.floor(Math.random() * productIds.length)];
-
-  const reviewsByProduct = http.get(`${url}/reviews?product_id=${id}`);
-  // console.log(`id: ${id}`);
-  check(reviewsByProduct, {
-    'Get status is 200': r => r.status === 200
+  const productMeta = http.get(`${url}/reviews/meta?product_id=${id}`);
+  check(productMeta, {
+    'Get status is 200': r => r.status === 200,
+    'Get valid product_id': r => r.status === 200 && r.json().product_id
   });
-
-  // const productMeta = http.get(`${url}/reviews/meta?product_id=${id}`);
-  // check(productMeta, {
-  //   'Get status is 200': r => r.status === 200,
-  //   'Get valid product_id': r => r.status === 200 && r.json().product_id
-  // });
   sleep(1);
 }
--- testing/k6reviews.test.js
+++ testing/k6reviews.test.js
@@ -2,18 +2,15 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 const productIds = require('../exampleData/productIds.js');
 
-// TODO
-// 1. Generate set of valid product_ids
-// 2. Create multiple scenarios using vus and sleeping
-// 3. Make test files for each get request
+const target = 10; // requests per second
 
 export const options = {
   scenarios: {
     stress: {
       executor: 'ramping-vus',
       stages: [
-        { duration: '5s', target: 10000 },
-        { duration: '10s', target: 10000 },
+        { duration: '5s', target },
+        { duration: '10s', target },
         { duration: '5s', target: 0 }
       ]
     }
@@ -26,14 +23,8 @@ export const options = {
 
 export default function () {
   const url = 'http://localhost:5000';
-
   const id = productIds[Math.floor(Math.random() * productIds.length)];
-
   const reviewsByProduct = http.get(`${url}/reviews?product_id=${id}`);
-
-  check(reviewsByProduct, {
-    'Get status is 200': r => r.status === 200
-  });
-
+  check(reviewsByProduct, { 'Get status is 200': r => r.status === 200 });
   sleep(1);
 }
